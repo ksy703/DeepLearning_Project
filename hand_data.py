476d0a27e165793735d6764745539740d0b76119
sy <ksy95703@naver.com>
2018-06-03 00:37:34 +0900

    new

--- hand_data.py
+++ hand_data.py
@@ -2,52 +2,52 @@ import numpy as np
 import cv2
 import csv
 
-imgD = cv2.imread('dataset5/D/a/depth_0_0422.png',1)
 
+for i in range(2,51):
+    if 10-i>0:
+        string='000%d'%i
+    elif 100-i>0:
+        string='00%d'%i
+    else :
+        string='0%d'%i
+    depth_image = 'dataset5/A/a/depth_0_'+string+'.png'
+    color_image = 'dataset5/A/a/color_0_'+string+'.png'
+    print(depth_image)
+    imgD = cv2.imread(depth_image, 1)
+    img_color = cv2.imread(color_image, cv2.IMREAD_COLOR)
+    img_ycrcb = cv2.cvtColor(img_color, cv2.COLOR_BGR2YCrCb)
+    img_gray = cv2.inRange(img_ycrcb, np.array([0, 133, 77]), np.array([255, 173, 127]))
 
-img_color = cv2.imread('dataset5/D/a/color_0_0422.png', cv2.IMREAD_COLOR)
-img_ycrcb=cv2.cvtColor(img_color,cv2.COLOR_BGR2YCrCb)
-img_gray=cv2.inRange(img_ycrcb,np.array([0,133,77]),np.array([255,173,127]))
+    for i in range(len(imgD)):
+        for j in range(len(imgD[0])):
 
-for i in range(len(imgD)):
-    for j in range(len(imgD[0])):
+            if (imgD[i][j][0] > 3):
+                img_gray[i][j] = 0
+            if (imgD[i][j][0] == 0):
+                img_gray[i][j] = 0
 
-        if(imgD[i][j][0]>3):
-            img_gray[i][j]=0
-        if(imgD[i][j][0]==0):
-            img_gray[i][j]=0
-        print(imgD[i][j])
-        print(img_gray[i][j])
+    _, img = cv2.threshold(img_gray, 60, 255, cv2.THRESH_BINARY)
+    img = cv2.morphologyEx(img, cv2.MORPH_ERODE, np.array((3, 3), np.uint8), iterations=2)
+    img_gray = cv2.morphologyEx(img, cv2.MORPH_OPEN, np.array([7, 7, 1]), iterations=2)
+    # img_gray=cv2.morphologyEx(img, cv2.MORPH_CLOSE,np.array([9, 9, 1]), iterations=2)
+    # cv2.medianBlur(img, 15)
+    hand_data = [[0 for i in range(128)] for j in range(128)]
+    for i in range(128):
+        for j in range(128):
+            if (len(img_gray) > i):
+                if (len(img_gray[0]) > j):
+                    hand_data[i][j] = img_gray[i][j]
+            print(hand_data[i][j])
 
-_,img=cv2.threshold(img_gray,60,255,cv2.THRESH_BINARY)
-img=cv2.morphologyEx(img, cv2.MORPH_ERODE, np.array((3,3),np.uint8), iterations=2)
-img_gray=cv2.morphologyEx(img, cv2.MORPH_OPEN, np.array([7, 7, 1]), iterations=2)
-#img_gray=cv2.morphologyEx(img, cv2.MORPH_CLOSE,np.array([9, 9, 1]), iterations=2)
-#cv2.medianBlur(img, 15)
+    cv2.imshow('depth', imgD);
 
-cv2.erode(img_gray,np.ones((5,5), np.uint8),1,2)
+    cv2.imshow('color image', img_color)
+    cv2.imshow('gray image', img_gray)
+    csvname=string+'.csv'
+    with open(csvname, 'w', newline='') as csvfile:
+        datawriter = csv.writer(csvfile, delimiter='\n')
+        datawriter.writerow(hand_data)
 
-_,contours,_=cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-max_area = 100
-ci = 0
-for i in range(len(contours)):
-    cnt = contours[i]
-    area = cv2.contourArea(cnt)
-    if (area > max_area):
-        max_area = area
-        ci = i
-
-cv2.drawContours(img_color,contours, ci, (0, 255, 255), 2)
-
-
-
-cv2.imshow('depth',imgD);
-
-
-cv2.imshow('color image', img_color)
-cv2.imshow('gray image', img_gray)
-
-cv2.imwrite('result.jpg', img_gray)
-
+    #cv2.imwrite('result.png', img_gray)
 cv2.waitKey(0)
-cv2.destroyAllWindow() 
+cv2.destroyAllWindow()
