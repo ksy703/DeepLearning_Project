e0dd099a79e55bbc5d3ec3a00b3189963cc1bef1
sy <ksy95703@naver.com>
2018-06-03 01:16:57 +0900

    finish

--- hand_data.py
+++ hand_data.py
@@ -10,9 +10,9 @@ for i in range(2,51):
         string='00%d'%i
     else :
         string='0%d'%i
-    depth_image = 'dataset5/A/a/depth_0_'+string+'.png'
-    color_image = 'dataset5/A/a/color_0_'+string+'.png'
-    print(depth_image)
+    depth_image = 'dataset5/A/p/depth_15_'+string+'.png'
+    color_image = 'dataset5/A/p/color_15_'+string+'.png'
+    print(string)
     imgD = cv2.imread(depth_image, 1)
     img_color = cv2.imread(color_image, cv2.IMREAD_COLOR)
     img_ycrcb = cv2.cvtColor(img_color, cv2.COLOR_BGR2YCrCb)
@@ -37,13 +37,13 @@ for i in range(2,51):
             if (len(img_gray) > i):
                 if (len(img_gray[0]) > j):
                     hand_data[i][j] = img_gray[i][j]
-            print(hand_data[i][j])
+
 
     cv2.imshow('depth', imgD);
 
     cv2.imshow('color image', img_color)
     cv2.imshow('gray image', img_gray)
-    csvname=string+'.csv'
+    csvname='handdata/A/p/'+string+'.csv'
     with open(csvname, 'w', newline='') as csvfile:
         datawriter = csv.writer(csvfile, delimiter='\n')
         datawriter.writerow(hand_data)
